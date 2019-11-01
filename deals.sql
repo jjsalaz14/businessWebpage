f842571147470351bbfbbc7fe8d233efc484d7c9
Juan J Salazar <juan_salazar96@hotmail.com>
2019-10-31 18:30:32 -0700

    Initial deals

---
+++ deals.sql
@@ -0,0 +1,16 @@
+use dev;
+
+-- INSERT INTO deals VALUES (1, "$10.00 Off", "12/31/19", "Buy up to 4 new tires and get $10 off each new tire.
+-- Limited to four tires per customer");
+
+-- INSERT INTO deals VALUES (2, "Free Tire", "11/14/19", "Buy three Michelin tires and get the fourth one for free. 
+-- Limited to one per customer");
+
+-- INSERT INTO deals VALUES (3, "$50.00 Reward Card", "11/08/19", "With the purchase of four new Toyo tires. 
+-- Limited to one for each customer");
+
+-- INSERT INTO deals VALUES (4, "$25.00/each Used Tires", "11/15/19", "For a limited time, all used 14 inch tires 
+-- $25.00 each. Limted to four for each customer");
+
+
+select * from deals;
