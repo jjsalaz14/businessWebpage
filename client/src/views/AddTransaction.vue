ffb98c7ce570ca3d35ba028ac932c6b935265681
Juan J Salazar <juan_salazar96@hotmail.com>
2020-02-11 21:03:55 -0800

    Add Item From Inventory To Cart

--- client/src/views/AddTransaction.vue
+++ client/src/views/AddTransaction.vue
@@ -5,7 +5,7 @@
                 <h1 style="font-size:200%; font-weight:bold">New Transaction:</h1>
                 <div class="new-transaction">
                     <form v-on:submit.prevent="addToCart()">
-                    <h1 style="font-size:120%; font-weight:bold"><u>Add New Transaction</u></h1>
+                    <h1 style="font-size:120%; font-weight:bold"><u>Create New Transaction</u></h1>
                     <br>
                     <p v-if="error" style="color:red">{{errorMsg}}</p>
                     <p>Type:</p> 
@@ -37,6 +37,70 @@
                     </form>
                 </div>
             </div>
+            
+            <div class="add-trans-inventory">
+                <h1 style="font-size:120%; font-weight:bold"><u>Select Item From Inventory</u>
+                    <p v-if="quantityError" style="color:red">{{quantityErrorMsg}}</p>
+                    <p v-if="addInvSuccess" style="color:green">{{addInvSuccessMsg}}</p>
+                </h1>
+                <br>
+            
+            <div class="filter-by">
+            <h1><u>Select One:</u></h1><br>
+            <p>Brand:</p> 
+              <select v-model="brandChoosen" >
+                <option
+                  v-for="selectedBrand in allBrand"
+                  v-bind:key="selectedBrand"
+                  :value="selectedBrand"
+                >{{selectedBrand}}</option>
+              </select>
+            <p>Size:</p> 
+              <select v-model="sizeChoosen" >
+                <option
+                  v-for="selectedSize in allSize"
+                  v-bind:key="selectedSize"
+                  :value="selectedSize"
+                >{{selectedSize}}</option>
+              </select>
+             <p>Category:</p> 
+              <select v-model="categoryChoosen" >
+                <option
+                  v-for="selectedCategory in allCategories"
+                  v-bind:key="selectedCategory"
+                  :value="selectedCategory"
+                >{{selectedCategory}}</option>
+              </select>
+              <div>
+                <button type="button" style="margin-left: 100px;border-radius: 12px;" v-on:click="showItems()">Select</button>
+              </div>
+            </div>
+
+            <div class="inventory">
+                <article v-for="(a, index) in inventory" v-bind:key="index">
+                    <div class="item-box">
+                        <img :src="a.image"  width="200" height="200">
+                        <div class="item-info">
+                            <p>Size:  {{a.size}}</p>
+                            <p>Brand:  {{a.brand}}</p>
+                            <p>Price:  ${{a.price}}</p>
+                            <p>Quantity: {{a.quantity}}</p>
+                            <p>{{a.category}}</p>
+                        </div>
+                        
+                        <form v-on:submit.prevent="addToCartFromInven(a.size, a.brand, a.price, a.quantity, a.category, index,a.key)">
+
+                            <p>Number Of Items To Add: <textarea rows="1" style="width:50px"  v-model="itemsToAdd" required></textarea></p>
+                            <button style="margin-left: 20px;border-radius: 12px;">Add To Cart</button>
+                        </form>
+                    </div>
+                </article>
+                <div v-if="inventory.length==0">
+                  <h1 style="padding-left: 100px; font-size: 24px">Select An Item</h1>
+                </div>
+            </div>
+
+            </div>
         </div>
 
         <div class="cart-list">
@@ -54,8 +118,11 @@
             <p v-if="transactions.length>0">Total Revenue = {{getRevenue()}}</p>
             <button v-if="transactions.length>0" @click="completeOrder" style="width:40%; margin-left: 213px; margin-top: 20px">Complete Order</button>
             <br><br><br><br><br>
-            </div>
         </div>
+
+ </div>
+
+        
 
 </template>
     
@@ -65,6 +132,7 @@ import axios, { AxiosResponse, AxiosError } from "axios";
 import { APIConfig } from "../utils/api.utils";
 import { Component, Prop, Vue } from "vue-property-decorator";
 import { iTransactions } from "../models/transactions.interface";
+import { iInventory } from "../models/inventory.interface";
 import { log } from "util";
 
 
@@ -73,14 +141,27 @@ export default class AddTransaction extends Vue {
     error: boolean = false;
     errorMsg: string = "";
     cartMsg: string = "No Items In Cart";
+    quantityError: boolean = false;
+    quantityErrorMsg: string = "Invalid Quantity";
+    addInvSuccess: boolean = false;
+    addInvSuccessMsg: string = "Item(s) Added To Cart";
     description: string = "";
     amount: number = 0;
+    itemsToAdd: number = 0;
     category: string = "";
     date: string = "";
     inventoryId: string = "NA";
     quantity: number = 0;
     services = [""];
     transactions: iTransactions[] = [];
+    inventory: iInventory[] = [];
+    allInventory: iInventory[] = [];
+    brandChoosen: string = "ALL";
+    categoryChoosen: string = "ALL";
+    sizeChoosen: string = "ALL";
+    allBrand = ["ALL"];
+    allCategories = ["ALL", "NEW", "USED", "WHEEL"];
+    allSize = ["ALL"];
 
     created() {
         this.getAllServices().then(data => {
@@ -92,6 +173,32 @@ export default class AddTransaction extends Vue {
             }
             this.services.shift();
         });
+
+        this.getInventory().then(data => {
+
+            for (let key in data){
+                this.inventory.push(data[String(key)]);
+            }
+            console.log(this.inventory);
+            this.allInventory = this.inventory;
+            this.inventory = [];
+        });
+    }
+
+    showItems(){
+        this.addInvSuccess = false;
+        this.quantityError = false;
+        this.inventory = [];
+        var i = 0;
+        for(i = 0; i < this.allInventory.length; i++){
+            if(this.brandChoosen == "ALL" || this.allInventory[i].brand.toUpperCase() == this.brandChoosen){
+                if(this.sizeChoosen == "ALL" || this.allInventory[i].size.slice(-2) == this.sizeChoosen){
+                    if(this.categoryChoosen == "ALL" || this.allInventory[i].category.toUpperCase() == this.categoryChoosen){
+                        this.inventory.push(this.allInventory[i]);
+                    }
+                }       
+            }      
+        }
     }
 
     getAllServices() {
@@ -111,6 +218,49 @@ export default class AddTransaction extends Vue {
 
             return servicesArray;
         })
+    }
+
+    getInventory() {
+        this.error = false;
+
+        var inventoryArray = [""];
+        var self = this;
+        
+
+        return axios.get('https://texanotireshop.firebaseio.com/inventory.json')
+        .then(function(data)  {
+            return data.data;}).then(function(data) {
+            var i: number = 0;
+            for (let key in data){
+            data[key].key = key;
+            data[key].initialIndex = i;
+            i = i + 1;
+            inventoryArray.push(data[key]);
+            self.allBrand.push(data[key].brand.toUpperCase());
+            self.allSize.push(data[key].size.slice(-2));
+            //console.log(key);
+            }
+            //Remove "" element in array
+            inventoryArray.shift();
+
+            self.allSize = self.uniqueItem(self.allSize);
+            self.allBrand = self.uniqueItem(self.allBrand);
+            console.log(self.allSize);
+        return inventoryArray;
+        
+        })
+    }
+
+    uniqueItem(array: any){
+      var uniqueArray = [""];
+      var i = 0;
+      for(i = 0; i < array.length; i++){
+        if(!uniqueArray.includes(array[i])){
+          uniqueArray.push(array[i]);
+        }
+      }
+      uniqueArray.shift();
+      return uniqueArray;
     }
 
     completeOrder(){
@@ -174,30 +324,45 @@ export default class AddTransaction extends Vue {
         else {
             this.error = false;
             console.log("Added to cart");
-            this.transactions.push(this.newTrans());
+            this.transactions.push(this.newTrans(this.description, this.amount, this. category, this.inventoryId, this.quantity));
 
             this.category = "";
             this.amount = 0;
             this.quantity = 0;
             this.description = "";
         }
+  }
 
+  addToCartFromInven(size: string , brand: string, price: number, quantity: number, 
+  category:string, index: number, key: string){
+      if(this.itemsToAdd <= 0 || this.itemsToAdd > quantity){
+          this.quantityError = true;
+          this.addInvSuccess = false;
+      }
+      else{
+          this.quantityError = false;
+          this.addInvSuccess = true;
+          this.transactions.push(this.newTrans(category+" "+brand+" "+size, price*this.itemsToAdd, "Expense", key, this.itemsToAdd));
+          this.allInventory[index].quantity -= this.itemsToAdd;
+      }
+      // itemsToAdd
   }
 
 
-  newTrans(): iTransactions {
+  newTrans(description: string, amount: number, category: string, inventoryId: string, 
+    quantity: number): iTransactions {
     let today = new Date();
     var dd = String(today.getDate()).padStart(2, '0');
     var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
     var yyyy = today.getFullYear();
 
     return {
-        description: this.description,
-        amount: Number(this.amount),
-        category: this.category,
+        description: description,
+        amount: Number(amount),
+        category: category,
         date: mm + '/' + dd + '/' + yyyy,
-        inventoryId: this.inventoryId,
-        quantity: Number(this.quantity)
+        inventoryId: inventoryId,
+        quantity: Number(quantity)
     }
 }
 
@@ -221,8 +386,40 @@ export default class AddTransaction extends Vue {
     .cart-list{
         padding-top: 112px;
         padding-left: 5px;
-        padding-right: 250px;
+        padding-right: 100px;
         float: right;
-}
+    }
+
+    .add-trans-inventory {
+        padding-top: 44px;
+        padding-left: 50px;
+        float: left;
+    }
+
+    .filter-by {
+      float: left;
+      border: 1px solid black;
+      padding: 20px;
+    }
+    .item-box {
+        width: 300px;
+        border: 1px solid rgb(187, 185, 185);
+        padding: 10px;
+        float: left;
+        margin: 15px;
+        background-color: rgb(184, 188, 190);
+        text-align: center;
+    }
+    .item-info {
+        border: 1px solid rgb(156, 153, 153);
+        padding: 10px;
+        text-align: left;
+    }
+    .inventory {
+        float: right;
+        float: below;
+        
+    }
+
 </style>
 
