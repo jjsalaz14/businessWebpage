9fc405fe126b8e8b93e9b6e5daab68fb9d29b907
Juan J Salazar <juan_salazar96@hotmail.com>
2020-03-03 14:54:28 -0800

    Login Cypress Testing

--- client/tests/e2e/specs/home_page_specs.js
+++ client/tests/e2e/specs/home_page_specs.js
@@ -1,12 +1,64 @@
-describe('My First Test', function(){
-    it('Finds An Element', function(){
-        cy.visit('http://localhost:8080/')
-        cy.contains('Username')
-        cy.contains('Password')
-        cy.contains('Inventory')
+describe('Home Page', function(){
+    it('Finds Shop Name and Phone Number', function(){
+        cy.visit('/')
 
-        cy.get('.username')
-
-        cy.get('.password')
+        cy.contains('Texano Tire Shop')
+        cy.contains('(760)123-4567')
     })
 })
+
+describe('Customer Contact Page', function(){
+    it('Finds Elements in Contact Page', function(){
+        cy.visit('/')
+        cy.contains('Contact').click()
+
+        cy.contains('(760)123-4567')
+        cy.contains('123 Main Street')
+        cy.contains('Friday: 8 AM - 5 PM')
+        cy.contains('Sunday: Closed')
+    })
+})
+
+describe('Customer Services Page', function(){
+    it('Finds Elements in Services Page', function(){
+        cy.visit('/')
+        cy.contains('Services').click()
+
+        cy.contains('New Tire')
+        cy.contains('Tire Installation')
+        cy.contains('Wheel Balance')
+        cy.contains('Check Tire Pressure')
+    })
+})
+
+
+describe('Login Unsuccessfully',()=>{
+    it('Login With Incorrect Credentials',function (){
+        cy.visit('/')
+
+        cy.contains('Log In').click()
+        .get('.username').type('student')
+        .get('.password').invoke('show').type('12345')
+        cy.contains('login').click()
+
+        cy.contains('Incorrect ID or Password')
+    })
+})
+
+describe('Login Successfully',()=>{
+    it('Login With Correct Credentials',function (){
+        cy.visit('/')
+
+        cy.contains('Log In').click()
+        .get('.username').type('admin')
+        .get('.password').invoke('show').type('password')
+        cy.contains('login').click()
+
+        cy.contains('owner')
+        cy.contains('Edit Services').click({force:true})
+        cy.contains('Log Out').click()
+
+        cy.contains('Log In').click()
+    })
+})
+
