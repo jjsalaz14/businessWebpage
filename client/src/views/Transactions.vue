5c9e1e4dbe20c41649a16588f48d00de7e15e6b5
Juan J Salazar <juan_salazar96@hotmail.com>
2020-03-06 18:05:24 -0800

    Filter Transactions By Start And End Date

--- client/src/views/Transactions.vue
+++ client/src/views/Transactions.vue
@@ -2,46 +2,12 @@
     <div v-if="isLoggedIn" class="transactions"> 
         <div class="description">
             <h1 style="font-size:200%; font-weight:bold">Transactions:</h1><br>
+            <p v-if="error" style="color:red">{{errorMsg}}</p>
             <div class="filter-by">
                 <h1><u>Filter By:</u></h1><br>
-                <p>Day:</p> 
-                    <select v-model="dayChoosen" >
-                        <option
-                            v-for="selectedDay in allDays"
-                            v-bind:key="selectedDay"
-                            :value="selectedDay"
-                            >{{selectedDay}}
-                        </option>
-                    </select>
-                <!-- <p>Week:</p> 
-                    <select v-model="weekChoosen" >
-                        <option
-                            v-for="selectedWeek in allWeeks"
-                            v-bind:key="selectedWeek"
-                            :value="selectedWeek"
-                            >{{selectedWeek}}
-                        </option>
-                    </select> -->
-                <p>Month:</p> 
-                    <select v-model="monthChoosen" >
-                        <option
-                            v-for="selectedMonth in allMonths"
-                            v-bind:key="selectedMonth"
-                            :value="selectedMonth"
-                            >{{MONTHS[Number(selectedMonth)]}}
-                        </option>
-                    </select>
-                <p>Year:</p> 
-                    <select v-model="yearChoosen" >
-                        <option
-                            v-for="selectedYear in allYears"
-                            v-bind:key="selectedYear"
-                            :value="selectedYear"
-                            >{{selectedYear}}
-                        </option>
-                    </select>
+
                 <p>Category:</p> 
-                    <select v-model="categoryChoosen" >
+                    <select v-model="categoryChoosen" @change="onChange()">
                         <option
                             v-for="selectedCategory in allCategories"
                             v-bind:key="selectedCategory"
@@ -49,9 +15,20 @@
                             >{{selectedCategory}}
                         </option>
                     </select>
+              <br>
               <div>
-                <button type="button" style="margin-left: 100px;border-radius: 12px;" v-on:click="filterItems()">Filter</button>
+                <form v-on:submit.prevent="filterItems()">
+                    <p>Start Date:</p>
+                    <textarea placeholder="MM/DD/YYYY" rows="2" style="width:120px" v-model="startDate" required ></textarea>
+                    <p>End Date:</p>
+                    <textarea placeholder="MM/DD/YYYY" rows="2" style="width:120px" v-model="endDate" required ></textarea>
+                    <div>
+                        <button type="button" style="margin-left: 0px;border-radius: 12px;" v-on:click="showAll()">Show All</button>
+                        <button style="margin-left: 20px;border-radius: 12px;">Filter</button>
+                    </div>
+                </form>
               </div>
+
             </div>
             <div style="padding-left:300px;">
                 <table id="data">
@@ -101,33 +78,29 @@ import axios, { AxiosResponse, AxiosError } from "axios";
 import { APIConfig } from "../utils/api.utils";
 import { iTransactions } from "../models/transactions.interface";
 import { Component, Prop, Vue } from "vue-property-decorator";
+import { log } from "util";
+import moment from 'moment';
 
 @Component({})
 export default class Transactions extends Vue {
     transactions: iTransactions[] = [];
     allTransactions: iTransactions[] = [];
-    dayChoosen: string = "ALL";
-    monthChoosen: string = "ALL";
-    yearChoosen: string = "ALL";
     categoryChoosen: string = "ALL";
-    weekChoosen: string = "ALL";
     totalRevenue: number = 0;
     totalCost: number = 0;
     totalProfit: number = 0;
-    allDays = ["ALL"];
-    allMonths = ["ALL"];
-    allYears = ["ALL"];
-    allWeeks = ["ALL"];
+    startDate: string = "";
+    endDate: string = "";
     allCategories = ["ALL", "Expense", "Revenue"]
-    MONTHS = ["ALL","January", "February", "March", "April", "May", "June",
-                "July", "August", "September", "October", "November", "December"]
+    errorMsg: string = "";
+    error: Boolean = false;
+
     created() {
         this.getTransactions().then(data => {
 
             for (let key in data){
                 this.transactions.push(data[String(key)]);
         }
-        console.log(this.allMonths);
         this.allTransactions = this.transactions;
         });
     }
@@ -144,20 +117,9 @@ export default class Transactions extends Vue {
             for (let key in data){
                 transArray.push(data[key]);
                 i =  i + 1;
-                self.allDays.push(data[key].date);
-                self.allMonths.push(data[key].date.slice(0,2));
-                self.allYears.push(data[key].date.slice(6,10));
-                
-                if(self.dayOfWeek(data[key].date)==1){
-                    self.allWeeks.push(data[key].date);
-                }
             }
             //Remove "" element in array
             transArray.shift();
-
-            self.allDays = self.uniqueItem(self.allDays);
-            self.allMonths = self.uniqueItem(self.allMonths);
-            self.allYears = self.uniqueItem(self.allYears);
 
         return transArray;
       
@@ -203,31 +165,64 @@ export default class Transactions extends Vue {
         return total;
     }
 
-    dayOfWeek(input: string){
-        let day = input.slice(3,5)
-        let month = this.MONTHS[Number(input.slice(0,2))];
-        let year = input.slice(6,10);
-        const dayString = new Date(month + ' ' + day + ',' + year + ' 00:00:01');
-        const dayNum = dayString.getDay();
-        return dayNum;
-    }
-
     filterItems(){
 
-        this.transactions = [];
-        var i = 0;
-        for(i = 0; i < this.allTransactions.length; i++){
-            console.log(this.monthChoosen);
-            if(this.monthChoosen == "ALL" || Number(this.allTransactions[i].date.slice(0,2)) == Number(this.monthChoosen)){
-                if(this.dayChoosen == "ALL" || this.allTransactions[i].date == this.dayChoosen){
-                    if(this.categoryChoosen == "ALL" || this.allTransactions[i].category == this.categoryChoosen){
-                        if(this.yearChoosen == "ALL" || this.allTransactions[i].date.slice(6,10) == this.yearChoosen){
+        if(!moment(this.startDate, "MM/DD/YYYY", true).isValid()){
+            this.error = true;
+            this.errorMsg = "Invalid Start Date";
+        }
+        else if(!moment(this.endDate, "MM/DD/YYYY", true).isValid()){
+            this.error = true;
+            this.errorMsg = "Invalid End Date";
+        }
+        else {
+
+            let start = new Date(this.startDate);
+            let end = new Date(this.endDate);
+            if(start>end){
+                this.error = true;
+                this.errorMsg = "End Date Should Be Greater Than Or Equal To Start Date"
+            }
+            else{
+                this.error = false;
+                this.errorMsg = "";
+
+                this.transactions = [];
+
+                var i = 0;
+                for(i = 0; i < this.allTransactions.length; i++){
+                    if(this.categoryChoosen == "ALL" || this.allTransactions[i].category == this.categoryChoosen){  
+                        let givenDay = new Date(this.allTransactions[i].date);
+                        if(start <= givenDay && givenDay <= end){
                             this.transactions.push(this.allTransactions[i]);
                         }   
                     }
                 }
             }
         }
+    }
+
+    onChange(){  
+        if(this.endDate!="" && this.startDate!= ""){
+            this.filterItems();
+        }
+        else {    
+            this.transactions = [];
+            var i = 0;
+            for(i = 0; i < this.allTransactions.length; i++){
+                if(this.categoryChoosen == "ALL" || this.allTransactions[i].category == this.categoryChoosen){
+                    this.transactions.push(this.allTransactions[i]);
+                }   
+            }
+        }
+    }
+
+    showAll(){
+        this.transactions = [];
+        this.transactions = this.allTransactions;
+        this.categoryChoosen = "ALL"
+        this.startDate = "";
+        this.endDate = "";
     }
 
     get isLoggedIn(): boolean {
